9543aa93a26acb7aa10df8cb2de4217fc87e782e
Jenesis Merriman <jenesishbm@berkeley.edu>
2024-02-17 19:36:16 -0800

    finished first attempt

--- lesson_2/practice.rb
+++ lesson_2/practice.rb
@@ -1,3 +1,7 @@
+# Practice Problems: Sorting, Nested Collections and Working with Blocks
+
+# ** = REVIEW
+
 # 1. How would you order this array of number strings by descending numeric value?
 arr = ['10', '11', '9', '7', '8']
 
@@ -29,3 +33,199 @@ arr2[1][:third][0]
 arr3[-1][:third][0][0]
 hsh1['b'][1]
 hsh2[:third].key(0) # returns key for given value
+
+# 4. For each of these collection objects where the value 3 occurs, demonstrate how you would change this to 4.
+
+arr1 = [1, [2, 3], 4]
+arr1[1][1] = 4
+
+arr2 = [{a: 1}, {b: 2, c: [7, 6, 5], d: 4}, 3]
+arr2[-1] = 4
+
+hsh1 = {first: [1, 2, [3]]}
+hsh1[:first][2][0] = 4
+
+hsh2 = {['a'] => {a: ['1', :two, 3], b: 4}, 'b' => 5}
+hsh2[['a']][:a][-1] = 4
+
+# 5. Determine the total age of just the male members of the family.
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_age = 0
+
+munsters.each_value do |hash|
+  total_age += hash["age"] if hash["gender"] == "male"
+end
+
+# 6. Given this previously seen family hash, print out the name, age and gender of each family member:
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, details|
+  puts "#{name} is a #{details["age"]} year-old #{details["gender"]}"
+end
+
+# **7. Given this code, what would be the final values of a and b?
+a = 2
+b = [5, 8]
+arr = [a, b]
+
+arr[0] += 2
+arr[1][0] -= a
+
+# a would reference 2 and b would reference [3, 8]. We do not reference a directly.
+
+# 8. Using the each method, write some code to output all of the vowels from the strings.
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+vowels = ["a", "e", "i", "o", "u"] # or "aeiou"
+
+hsh.each_value do |strings|
+  strings.each do |word|
+    word.chars.each do |letter|
+      puts letter if vowels.include?(letter)
+    end
+  end
+end
+
+# 9. Given this data structure, return a new array of the same structure but with the sub arrays being ordered (alphabetically or numerically as appropriate) in descending order.
+arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
+
+arr.each do |sub_arr|
+  sub_arr = sub_arr.sort!.reverse!
+end
+
+# OR LS:
+
+arr.map do |sub_arr|
+  sub_arr.sort do |a, b|
+    b <=> a
+  end
+end
+
+# **10. Given the following data structure and without modifying the original array, use the map method to return a new array identical in structure to the original but where the value of each integer is incremented by 1.
+
+arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+
+arr.map do |hash|
+  new = {}
+  hash.each do |key, value|
+    new[key] = value + 1
+  end
+  new
+end
+
+# 11. Given the following data structure use a combination of methods, including either the select or reject method, to return a new array identical in structure to the original but containing only the integers that are multiples of 3.
+arr = [[2], [3, 5, 7, 12], [9], [11, 13, 15]]
+
+arr.map do |sub_arr|
+  sub_arr.select do |num|
+    num % 3 == 0
+  end
+end
+
+# **12. Given the following data structure, and without using the Array#to_h method, write some code that will return a hash where the key is the first item in each sub array and the value is the second item.
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+
+hash = {}
+arr.each do |item|
+  hash[item[0]] = item[1]
+end
+
+hash # => {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
+
+# **13. Given the following data structure, return a new array containing the same sub-arrays as the original but ordered logically by only taking into consideration the odd numbers they contain.
+arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
+
+# my attempt (wrong, error)
+arr.map do |sub_arr|
+  sub_arr.sort_by {|element| element.odd? }
+end
+
+# LS
+arr.sort_by do |sub_arr|
+  sub_arr.select do |num|
+    num.odd?
+  end
+end
+
+# 14. Given this data structure write some code to return an array containing the colors of the fruits, and the sizes of the vegetables. The sizes should be uppercase and the colors should be capitalized.
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+
+arr = []
+
+hsh.each_value do |details|
+  if details[:type] == 'fruit'
+    arr << details[:colors].map { |color| color.capitalize }
+  else
+    arr << details[:size].upcase
+  end
+end
+
+arr
+
+# **15. Given this data structure write some code to return an array which contains only the hashes where all the integers are even.
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+
+arr.select do |hash|
+  hash.all? do |_, arr|
+    arr.all? {|num| num.even? }
+  end
+end
+
+# get more practice with nested iterator methods, pay attention to what they return
+
+# **16. Write a method that returns one UUID when called with no parameters.
+
+# my solution - works but not so clean
+def generate_uuid
+  hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
+  uuid = []
+  8.times { uuid << hex.sample }
+  uuid << "-"
+  4.times { uuid << hex.sample }
+  uuid << "-"
+  4.times { uuid << hex.sample }
+  uuid << "-"
+  4.times { uuid << hex.sample }
+  uuid << "-"
+  12.times { uuid << hex.sample }
+  uuid.join
+end
+
+generate_uuid
+
+# LS solution
+def generate_UUID
+  characters = []
+  (0..9).each { |digit| characters << digit.to_s }
+  ('a'..'f').each { |digit| characters << digit }
+
+  uuid = ""
+  sections = [8, 4, 4, 4, 12]
+  sections.each_with_index do |section, index|
+    section.times { uuid += characters.sample }
+    uuid += '-' unless index >= sections.size - 1
+  end
+
+  uuid
+end
+
+#Each UUID consists of 32 hexadecimal characters, and is typically broken into 5 sections like this 8-4-4-4-12 and represented as a string.
