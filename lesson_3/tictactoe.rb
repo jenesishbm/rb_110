4c4d62609e33bb9eaefb4f659675885e72c7ae94
Jenesis Merriman <jenesishbm@berkeley.edu>
2024-02-18 17:55:15 -0800

    tic tac toe program

---
+++ lesson_3/tictactoe.rb
@@ -0,0 +1,130 @@
+# Tic Tac Toe Program
+
+=begin
+Outline sequence
+  1. Display the initial empty 3x3 board
+  2. Ask the user to mark a square
+  3. Computer marks a square
+  4. Display updated board
+  5. Repeat 2-4 until someone marks 3 in a row or board is full
+  6. If someone wins, display winner
+  7. If board is full, display tie
+  8. Play again?
+  9. If yes, go to #1
+  10. Goodbye!
+=end
+
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]] # diagonals
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def display_board(brd)
+  system 'clear'
+  puts "You are #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  # creates hash with square numbers and corresponding mark
+  new_board
+end
+
+def empty_squares(brd)
+  # binding.pry
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def player_move!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    # break if the keys with empty squares include the user choice
+    prompt "Sorry, that's not a valid choice."
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_move!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd) # false if nil, true if someone won
+end
+
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+      return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+# Game begins
+
+loop do
+  board = initialize_board # sets board to our empty board
+  display_board(board)
+
+  loop do
+    display_board(board)
+
+    player_move!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_move!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing! Goodbye."
